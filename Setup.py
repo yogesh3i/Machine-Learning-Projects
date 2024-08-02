042d1a0200188797ffebab165ffef2148f3b7d96
Yogesh <yogideotale33@gmail.com>
2024-08-02 11:45:40 +0530

    'EDA_Model'

--- Setup.py
+++ Setup.py
@@ -1,15 +1,22 @@
 from setuptools import find_packages,setup 
 
 from typing import List
+
+HYPEN_E_DOT = '-e'
 def get_requirements(file_path:str)->List[str]:
     '''
     This function willl return the list of requirements
     '''
     requirements=[]
+    
     with open(file_path) as file_obj:
-        
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
+    return requirements 
 
 
 
@@ -17,9 +24,10 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='ML_Project',
     version ='0.0.1',
-    author='Yogesh7',
+    author='Yogesh  ',
     author_email='yogideotale33@gmail.com',
     packages=find_packages(),
     install_requires=['pandas','numpy','seaborn','matplotlib'],
     instal_requires=get_requirements('requirements.txt')
 )
+
